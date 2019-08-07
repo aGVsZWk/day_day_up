c323e3830b8babb5c63e22eae6740a5150c74a3e
helei <helei@jhbytech.com>
2019-08-07 16:32:40 +0800

    完成5.4 对重复代码进行抽取

--- TDD-with-python/codes/superlists/functional_tests.py
+++ TDD-with-python/codes/superlists/functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.close()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_case_start_a_list_and_retrieve_it_later(self):
         # 测试首页标题和头部包含"To-Do"这个词
         self.browser.get('http://127.0.0.1:8001')
@@ -30,9 +35,7 @@ class NewVisitorTest(unittest.TestCase):
         # 输入回车, 页面显示更新, 在待办事项列表中显示了'Buy peacock feathers'
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 再输入其它的待办事项并判断
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -46,11 +49,11 @@ class NewVisitorTest(unittest.TestCase):
         # 输入回车, 页面显示更新, 在待办事项列表中显示了'Buy peacock feathers'
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Use peacock feathers to make a fly', [
-                row.text for row in rows]
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly'
             )
+
         self.fail('Finish the test')
         # 可以输入其他内容, 在做其它的事情
 
