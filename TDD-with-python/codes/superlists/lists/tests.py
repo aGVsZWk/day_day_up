27ae04b45f5e40271894aa4d84ed717937c98304
qq905713813@163.com <qq905713813@163.com>
2019-08-07 23:12:13 +0800

    完成5.8-在模板中渲染待办事项

--- TDD-with-python/codes/superlists/lists/tests.py
+++ TDD-with-python/codes/superlists/lists/tests.py
@@ -34,6 +34,14 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
